064c9364e049cb689c331aeecd1b15c6d18042e0
CLYDE <bazileclyde@gmail.com>
2017-03-07 00:09:54 -0500

    Fix all compilation issues

--- Documents/Automation/password.py
+++ Documents/Automation/password.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import sys, re
     
 """
@@ -10,37 +12,37 @@ import sys, re
     1 lowercase letter or more
     """
 
-    password = sys.argv[0]
+password = sys.argv[1]
 
-    # calcules the length
-    length_error = len(password) < 8
+# calcules the length
+length_error = len(password) < 8
 
-    # search for digits
-    digit_error = re.search(r"\d", password) is None
+# search for digits
+digit_error = re.search(r"\d", password) is None
 
-    # search for uppercase
-    uppercase_error = re.search(r"[A-Z]", password) is None
+# search for uppercase
+uppercase_error = re.search(r"[A-Z]", password) is None
 
-    # search for lowercase
-    lowercase_error = re.search(r"[a-z]", password) is None
+# search for lowercase
+lowercase_error = re.search(r"[a-z]", password) is None
 
-    # search for symbols
-    symbol_error = re.search(r"[ !#$%&'()*+,-./[\\\]^_`{|}~"+r'"]', password) is None
+# search for symbols
+symbol_error = re.search(r"[ !#$%&'()*+,-./[\\\]^_`{|}~"+r'"]', password) is None
 
-    # overall result
-    password_ok = not ( length_error or digit_error or uppercase_error or lowercase_error or symbol_error )
+# overall result
+password_ok = not ( length_error or digit_error or uppercase_error or lowercase_error or symbol_error )
 
-    if not password_ok :
-        print "password must contain: "
-        if length_error :
-		print "8 characters"
-	if digit_error :
-		print "one digit"
-	if uppercase_error :
-		print "uppercase letter"
-	if lowercase_error :
-		print "lowercase letter"
-	if symbol_error
-		print "special symbol"
-    else :
-	print "password accepted"
+if not password_ok :
+    print "password must contain: "
+    if length_error :
+	   print "\tat least eight characters"
+    if digit_error :
+	   print "\tat least one digit"
+    if uppercase_error :
+	   print "\tat least one uppercase letter"
+    if lowercase_error :
+	   print "\tat least one lowercase letter"
+    if symbol_error :
+	   print "\tat least one special symbol"
+else :
+    print "password accepted"
